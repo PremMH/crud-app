cdb39856e858d8d9136244db2c97d78f9d0f5e72
prem.hagaragi <prem.hagaragi@minsyst.com>
2024-05-19 22:18:29 +0530

    latest changes

--- controller/userController.js
+++ controller/userController.js
@@ -12,8 +12,79 @@ const addUser = async (req, res) => {
     }
 }
 
+const getUsers = async (req, res) => {
+    try {
+        const item = await User.find();
+        res.status(200).json(item);
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+}
+
+
+const getUser = async (req, res) => {
+
+    const {id} = req.params;
+
+    try {
+        const item = await User.findById(id);
+        res.status(200).json(item);
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+}
+
+
+const updateUser = async (req, res) => {
+
+    const { id } = req.params;
+    const updates = req.body;
+  
+    try {
+      const item = await User.findByIdAndUpdate(id, updates);
+  
+      if (!item) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+  
+      const updatedItem = await User.findById(id);
+      res.status(200).json({message: 'User updated sucessfully', data: updatedItem})
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  
+}
+
+
+const deleteUser = async (req, res) => {
+
+    const { id } = req.params;
+    const updates = req.body;
+  
+    try {
+      const item = await User.findByIdAndDelete(id);
+  
+      if (!item) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+  
+      const updatedItem = await User.findById(id);
+      res.status(200).json({message: 'User deleted sucessfully', data: updatedItem})
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  
+}
 
 
 module.exports = {
-    addUser
+    addUser,
+    getUsers,
+    getUser,
+    deleteUser,
+    updateUser
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,19 @@
 const express = require('express');
-
 const router = express.Router();
-
-
-const {addUser} = require('../controller/userController')
-
+const {addUser, getUser, updateUser, deleteUser, getUsers} = require('../controller/userController')
 
 
 
 router.post('/', addUser);
+
+router.get('/:id', getUser);
+
+router.get('/', getUsers);
+
+router.patch('/:id', updateUser);
+
+router.delete('/:id', deleteUser);
+
 
 
 
